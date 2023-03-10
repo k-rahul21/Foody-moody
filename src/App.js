40cae6387e1262343a3b243eedf728e532bf1bd4
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2023-03-10 12:58:21 +0530

    define: routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import  ReactDOM  from "react-dom/client";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Body from "./components/Body/Body";
+import AboutUs from "./components/AboutUs/AboutUs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -14,10 +16,21 @@ const AppLayout = () => {
   )
 }
 
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>
+  },
+  {
+    path: "/about",
+    element: <AboutUs/>
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // passing a react element inside the root
 
 //async defer
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRoute}/>);
