2c61078aed81835e19c3d09d79674ac2a10cd78e
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2023-03-10 14:46:19 +0530

    add: error page

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,8 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const err = useRouteError();
+  const {status, statusText} = err;
   return (
     <div>
       <h1>Oops!!</h1>
       <h3>Something went wrong!!</h3>
+      <p>Status : {status} , Status Text : {statusText} </p>
     </div>
   )
 }
